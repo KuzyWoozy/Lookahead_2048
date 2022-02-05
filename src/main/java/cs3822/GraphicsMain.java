5f2cd5e3f4fa961d0ae1e69bb1f7aa4104d6e6e8
Daniil <dkuznetsov17@gmail.com>
2022-02-05 00:26:34 +0000

    Recompiled with new parameters

--- src/main/java/cs3822/GraphicsMain.java
+++ src/main/java/cs3822/GraphicsMain.java
@@ -107,7 +107,7 @@ public class GraphicsMain extends Application {
         }
 
         GameStats stats = new GameStats();
-        for (int x = 0; x < 100; x++) {
+        for (int x = 0; x < 300; x++) {
           stats.merge(view.play(manager, algo));
           manager.restart();
         }
